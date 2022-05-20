a66ef70824cccbf6afe821b9437e5031d01e57cd
JeroenR123 <jeroenraap@gmail.com>
2022-05-20 23:02:30 +0200

    First blog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import BlogPost from "./Component.jsx/BlogPost"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div data-theme='dark'>
+      <div className='w-screen h-screen flex items-center'>
+        <div className="bg-primary artboard w-10/12 h-min my-auto mx-auto rounded-md"><BlogPost /></div>
+        
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  daisyui: {
+    themes: ["cupcake", "dark", "cmyk"],
+  },
+  plugins: [require("daisyui")],
+}
